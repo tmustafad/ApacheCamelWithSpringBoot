f31827a6ca95b75251b8f5f4561e82e0380ecf15
Turkmen Mustafa Demirci <tmustafad@gmail.com>
2018-06-11 11:25:06 +0300

    code cleaning_deleting commented unnecessary parts

--- src/main/java/com/turkmen/camel/ApacheCamelWithSpringBoot/route/UserRouter.java
+++ src/main/java/com/turkmen/camel/ApacheCamelWithSpringBoot/route/UserRouter.java
@@ -13,24 +13,8 @@ import org.springframework.stereotype.Component;
 public class UserRouter extends AbstractRouter {
 
 	{
-		this.queueName="amqp:TURKMEN_USER";
-		this.urlName="user";
+		this.queueName = "amqp:TURKMEN_USER";
+		this.urlName = "user";
 	}
-	
-	
-//	@Override
-//	public void configure() throws Exception {
-//
-//		restConfiguration().component("servlet").bindingMode(RestBindingMode.json);
-//
-//		rest("/user").produces("application/json").get("{name}").route().transform()
-//				.simple("Hello ${header.name}, fenerbahce").to(ExchangePattern.InOnly, "amqp:TURKMEN_TEST")
-//				.process(exchange -> {
-//					exchange.getIn().setBody("{'success':'success'}");
-//					exchange.getIn().setHeader(Exchange.HTTP_RESPONSE_CODE, 200);
-//					exchange.getIn().setHeader(Exchange.CONTENT_TYPE, "application/json");
-//				});
-//
-//	}
 
 }
